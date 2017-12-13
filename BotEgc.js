47493b62d37dca04f914737f987f5d262307c7b3
thepema <pema7284@gmail.com>
2017-12-13 16:31:21 +0100

    Borrado de ficheros innecesarios2

--- BotEgc.js
+++
@@ -1,9 +0,0 @@
-var TelegramBot = require('node-telegram-bot-api');
-
-var token = '489385493:AAHABtpV9COcsOfgvhJM7WLy32TMps_PDiI';
-
-var bot = new TelegramBot(token, {polling: true});
-bot.on('text', function (msg) {
-  var chatId = msg.chat.id;
-  bot.sendMessage(chatId,msg.from.first_name + " ha enviado " + msg.text);
-});
